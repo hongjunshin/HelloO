70e0d26169fd0cda5c36d4c26ed6b4d7ef94da55
HongJunShin21700407 <21700407@handong.edu>
2021-09-03 13:24:06 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -7,7 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
-
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+"]님 환영합니다~");
+		s.close();
 	}
 
 }
